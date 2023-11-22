c0e5d6f5603cb4a660989e62ead72c59a59fa3af
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-11-22 21:40:39 +0100

    cors

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://lauradauzat.github.io/mrs-capuche/", 
-  publicDir:"https://lauradauzat.github.io/mrs-capuche/public/",
+  base:"www.mrscapuche.fr", 
+  publicDir:"www.mrscapuche.fr/public/",
 })
